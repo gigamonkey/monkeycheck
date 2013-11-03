d48a607284de436557865c28dbb3b6d7c7fd65cf
Peter Seibel <peter@gigamonkeys.com>
2013-11-03 12:32:30 -0800

    Starting to get a handle on correct shrinking.

--- MonkeyCheck.scala
+++ MonkeyCheck.scala
@@ -11,56 +11,44 @@ object MonkeyCheck {
 
   case class Parameters(size: Int, random: Random)
 
-  // Generators generate arbitrary inputs to properties.
-  type Generator[+T] = Parameters => Option[T]
-
-  trait G[T] extends (Parameters => Option[T]) {
+  trait Generator[T] extends (Parameters => Option[T]) {
 
     def apply(p: Parameters): Option[T]
 
     def shrink(value: T): Stream[T] = Stream.empty
 
-    def map[U](fn: T => U): G[U] = new G[U] {
-      def apply(p: Parameters) = G.this.apply(p).map(fn)
+    def map[U](fn: T => U): Generator[U] = new Generator[U] {
+      def apply(p: Parameters) = Generator.this(p).map(fn)
     }
 
-    def flatMap[U](fn: T => G[U]): G[U] = new G[U] {
-      def apply(p: Parameters) = G.this.apply(p).flatMap(fn(_).apply(p))
+    def flatMap[U](fn: T => Generator[U]): Generator[U] = new Generator[U] {
+      def apply(p: Parameters) = Generator.this(p).flatMap(fn(_)(p))
     }
 
-    def filter(fn: T => Boolean): G[T] = new G[T] {
-      def apply(p: Parameters) = G.this.apply(p).filter(fn)
-      override def shrink(value: T) = G.this.shrink(value).filter(fn)
+    def filter(fn: T => Boolean): Generator[T] = new Generator[T] {
+      def apply(p: Parameters) = Generator.this.apply(p).filter(fn)
+      override def shrink(value: T) = Generator.this.shrink(value).filter(fn)
     }
 
+    def shrinkWith(fn: T => Stream[T]) = new Generator[T] {
+      def apply(p: Parameters) = Generator.this.apply(p)
+      override def shrink(value: T) = fn(value)
+    }
   }
-
-  implicit class RichGenerator[T](g: Generator[T]) {
-    def map[U](fn: T => U): Generator[U]                = { ps => g(ps).map(fn) }
-    def flatMap[U](fn: T => Generator[U]): Generator[U] = { ps => g(ps).flatMap(fn(_)(ps)) }
-    def filter(fn: T => Boolean): Generator[T]          = { ps => g(ps).filter(fn) }
-  }
-
 
   // Public methods for getting generators.
 
   def arbitrary[T](ps: Parameters)(implicit g: Generator[T]): Option[T] = g.apply(ps)
-
   def generator[T](implicit g: Generator[T]): Generator[T] = g
   def between[T](min: T, max: T)(implicit b: Between[T]): Generator[T] = b(min, max)
   def oneOf[T](xs: Seq[T]): Generator[T] = between(0, xs.size - 1).map(xs(_))
   def oneOf[T](t: T, ts: T*): Generator[T] = oneOf[T](t +: ts)
-
-  def betwixt[T](min: T, max: T, p: Parameters)(implicit b: Between[T]): Option[T] = b(min, max)(p)
+  def const[T](t: T) = new Generator[T] { def apply(p: Parameters) = Some(t) }
 
   // Arbitrary generators are the base case generators for when all we
   // know about what we want is the type. If you want to have them
   // accessible, you'll want to import MonkeyCheck.Arbitrary._
   object Arbitrary {
-
-    implicit lazy val byteG = new G[Byte] {
-      def apply(p: Parameters) = betwixt(Byte.MinValue, Byte.MaxValue, p)
-    }
 
     implicit lazy val arbitraryByte:    Generator[Byte]    = between(Byte.MinValue, Byte.MaxValue)
     implicit lazy val arbitraryChar:    Generator[Char]    = between(Char.MinValue, Char.MaxValue)
@@ -69,60 +57,80 @@ object MonkeyCheck {
     implicit lazy val arbitraryLong:    Generator[Long]    = between(Long.MinValue, Long.MaxValue)
     implicit lazy val arbitraryFloat:   Generator[Float]   = between(Float.MinValue, Float.MaxValue)
     implicit lazy val arbitraryDouble:  Generator[Double]  = between(Double.MinValue, Double.MaxValue)
-    implicit lazy val arbitraryBoolean: Generator[Boolean] = { ps => Some(ps.random.nextBoolean) }
-    implicit lazy val arbitraryUnit:    Generator[Unit]    = { ps => Some(()) }
+    implicit lazy val arbitraryBoolean: Generator[Boolean] = oneOf(true, false)
+    implicit lazy val arbitraryUnit:    Generator[Unit]    = const(())
 
-    implicit lazy val arbitraryAnyVal: Generator[AnyVal] = { ps =>
-      oneOf(
-        arbitraryByte, arbitraryChar, arbitraryShort, arbitraryInt, arbitraryLong,
-        arbitraryFloat, arbitraryDouble, arbitraryBoolean, arbitraryUnit)
-        .apply(ps)
-        .flatMap(_.apply(ps))
+    implicit lazy val arbitraryAnyVal:  Generator[AnyVal] = new Generator[AnyVal] {
+      def apply(p: Parameters) =
+        oneOf(
+          arbitraryByte, arbitraryChar, arbitraryShort, arbitraryInt, arbitraryLong,
+          arbitraryFloat, arbitraryDouble, arbitraryBoolean, arbitraryUnit)
+          .apply(p).flatMap(_.apply(p))
     }
 
     // In scalacheck, the arbitrary option goes through some
     // machinations with the size. At least part of the point, I
     // think, is that we probably want to mostly return Somes whereas
     // this returns None half the time.
-    implicit def arbitraryOption[T:Generator]: Generator[Option[T]] = { ps =>
-      if (ps.random.nextBoolean) None else Some(generator[T].apply(ps)) }
+    implicit def arbitraryOption[T:Generator]: Generator[Option[T]] = new Generator[Option[T]] {
+      def apply(p: Parameters) = if (p.random.nextBoolean) None else Some(generator[T].apply(p))
+    }
 
-    implicit def arbitraryEither[T:Generator, U:Generator]: Generator[Either[T, U]] = { ps =>
-      if (ps.random.nextBoolean) {
-        arbitrary[T](ps).map(Left(_))
-      } else {
-        arbitrary[U](ps).map(Right(_))
-      }
+    implicit def arbitraryEither[T:Generator, U:Generator]: Generator[Either[T, U]] = new Generator[Either[T, U]] {
+      def apply(p: Parameters) =
+        if (p.random.nextBoolean) {
+          arbitrary[T](p).map(Left(_))
+        } else {
+          arbitrary[U](p).map(Right(_))
+        }
     }
 
     ////////////////////////////////////////////////////////////////////////
     // Tuples
 
-    implicit def arbitraryTuple1[T:Generator] =
-      for { t <- generator[T] } yield Tuple1(t)
+    implicit def arbitraryTuple1[T1:Generator] =
+      (for {
+        t1 <- generator[T1]
+      } yield Tuple1(t1)).shrinkWith {
+        case Tuple1(t1) =>
+          generator[T1].shrink(t1).map(Tuple1(_))
+      }
 
     implicit def arbitraryTuple2[T1:Generator, T2:Generator] =
-      for {
+      (for {
         t1 <- generator[T1]
         t2 <- generator[T2]
-      } yield (t1, t2)
+      } yield (t1, t2)).shrinkWith {
+        case (t1, t2) =>
+          generator[T1].shrink(t1).map((_, t2)) append
+          generator[T2].shrink(t2).map((t1, _))
+      }
 
-    implicit def arbitraryTuple3[T1:Generator, T2:Generator, T3:Generator] =
-      for {
+    implicit def arbitraryTuple2[T1:Generator, T2:Generator, T3:Generator] =
+      (for {
         t1 <- generator[T1]
         t2 <- generator[T2]
         t3 <- generator[T3]
-      } yield (t1, t2, t3)
+      } yield (t1, t2, t3)).shrinkWith {
+        case (t1, t2, t3) =>
+          generator[T1].shrink(t1).map((_, t2, t3)) append
+          generator[T2].shrink(t2).map((t1, _, t3)) append
+          generator[T3].shrink(t3).map((t1, t2, _))
+      }
 
-    // In some sense the previous generator knows how it generated the
-    // three parts of the tuple. To make a stream of shrunk tuples
-    // from a particular tuple it needs to make one stream for each
-    // element with values that contain a shrunk version of that
-    // element and the other two as in the original tuple being
-    // shrunk. Is this generally applicable? I.e. can the output of a
-    // generator that has been created by flatMapping always be
-
-
+    implicit def arbitraryTuple4[T1:Generator, T2:Generator, T3:Generator, T4:Generator] =
+      (for {
+        t1 <- generator[T1]
+        t2 <- generator[T2]
+        t3 <- generator[T3]
+        t4 <- generator[T4]
+      } yield (t1, t2, t3, t4)).shrinkWith {
+        case (t1, t2, t3, t4) =>
+          generator[T1].shrink(t1).map((_, t2, t3, t4)) append
+          generator[T2].shrink(t2).map((t1, _, t3, t4)) append
+          generator[T3].shrink(t3).map((t1, t2, _, t4)) append
+          generator[T4].shrink(t4).map((t1, t2, t3, _))
+      }
 
     // TODO: tuples up to 22
 
@@ -136,15 +144,15 @@ object MonkeyCheck {
     // satisfies those properties. Thus if you have some code you
     // actually need to test that takes a function as an argument
 
-    implicit def arbitraryFunction1[T1,R:Generator]: Generator[T1 => R] = { ps =>
-      for { r <- arbitrary[R](ps) } yield (t1: T1) => r
+    implicit def arbitraryFunction1[T1,R:Generator]: Generator[T1 => R] = new Generator[T1 => R] {
+      def apply(p: Parameters) = for { r <- arbitrary[R](p) } yield (t1: T1) => r
     }
 
-    implicit def arbitraryFunction2[T1,T2,R:Generator]: Generator[(T1, T2) => R] = { ps =>
-      for { r <- arbitrary[R](ps) } yield (t1: T1, t2: T2) => r
+    implicit def arbitraryFunction2[T1,T2,R:Generator]: Generator[(T1, T2) => R] = new Generator[(T1, T2) => R] {
+      def apply(p: Parameters) = for { r <- arbitrary[R](p) } yield (t1: T1, t2: T2) => r
     }
-
   }
+
   object Between {
     // Used internally to make generators for ordered things like numbers.
     type Between[T] = (T, T) => Generator[T]
@@ -157,13 +165,27 @@ object MonkeyCheck {
       if (low <= high) Some(low + math.abs(random % ((high - low) + 1))) else None
     }
 
-    implicit val betweenByte:   Between[Byte]   = (low: Byte, high: Byte)     => { ps => chooseLong(low, high, ps.random.nextLong).map(_.toByte) }
-    implicit val betweenChar:   Between[Char]   = (low: Char, high: Char)     => { ps => chooseLong(low, high, ps.random.nextLong).map(_.toChar) }
-    implicit val betweenShort:  Between[Short]  = (low: Short, high: Short)   => { ps => chooseLong(low, high, ps.random.nextLong).map(_.toShort) }
-    implicit val betweenInt:    Between[Int]    = (low: Int, high: Int)       => { ps => chooseLong(low, high, ps.random.nextLong).map(_.toInt) }
-    implicit val betweenLong:   Between[Long]   = (low: Long, high: Long)     => { ps => chooseLong(low, high, ps.random.nextLong) }
-    implicit val betweenFloat:  Between[Float]  = (low: Float, high: Float)   => { ps => chooseDouble(low, high, ps.random.nextDouble).map(_.toInt) }
-    implicit val betweenDouble: Between[Double] = (low: Double, high: Double) => { ps => chooseDouble(low, high, ps.random.nextDouble).map(_.toInt) }
+    implicit val betweenByte: Between[Byte] = (low: Byte, high: Byte) => new Generator[Byte] {
+      def apply(ps: Parameters) = chooseLong(low, high, ps.random.nextLong).map(_.toByte)
+    }
+    implicit val betweenChar: Between[Char] = (low: Char, high: Char) => new Generator[Char] {
+      def apply(ps: Parameters) = chooseLong(low, high, ps.random.nextLong).map(_.toChar)
+    }
+    implicit val betweenShort: Between[Short] = (low: Short, high: Short) => new Generator[Short] {
+      def apply(ps: Parameters) = chooseLong(low, high, ps.random.nextLong).map(_.toShort)
+    }
+    implicit val betweenInt: Between[Int] = (low: Int, high: Int) => new Generator[Int] {
+      def apply(ps: Parameters) = chooseLong(low, high, ps.random.nextLong).map(_.toInt)
+    }
+    implicit val betweenLong: Between[Long] = (low: Long, high: Long) => new Generator[Long] {
+      def apply(ps: Parameters) = chooseLong(low, high, ps.random.nextLong)
+    }
+    implicit val betweenFloat: Between[Float] = (low: Float, high: Float) => new Generator[Float] {
+      def apply(ps: Parameters) = chooseDouble(low, high, ps.random.nextDouble).map(_.toInt)
+    }
+    implicit val betweenDouble: Between[Double] = (low: Double, high: Double) => new Generator[Double] {
+      def apply(ps: Parameters) = chooseDouble(low, high, ps.random.nextDouble).map(_.toInt)
+    }
 
   }
 
