5e08e2cc44f9f52a2da68c3f66c1090a8d4c9e88
Peter Seibel <peter@gigamonkeys.com>
2013-11-09 18:39:21 -0800

    Basic property checking working.

--- MonkeyCheck.scala
+++ MonkeyCheck.scala
@@ -1,7 +1,8 @@
-package org.monkeycheck
+package com.gigamonkeys.monkeycheck
 
-import scala.util.Random
 import scala.annotation.tailrec
+import scala.language.implicitConversions
+import scala.util.Random
 
 object MonkeyCheck {
 
@@ -20,45 +21,72 @@ object MonkeyCheck {
   case class Falsified[T](value: T) extends Evidence[T]
   case class Proved[T](value: T) extends Evidence[T]
 
-
   // Checking a Property many times yields a result. While we are
-  // checking the result may be Unknown which keeps a count of times
-  // we've seen Support vs the total number of checks. If the
-  // Property is not proven or disproven then we will determine
-  // whether it passed or failed based on the ratio of support to
-  // checks.
-  sealed trait Result
-  case class Unknown(support: Int, noSupport: Int) extends Result
-  case object Passed extends Result
-  case object Failed extends Result
+  // checking, the result may be Unknown, in which case we keep a
+  // count of times we've seen Support vs the total number of
+  // checks. If the Property is not proven or disproven after we have
+  // done all our checks, then we will convert an Unknown to a pass or
+  // fail based on the ratio of support to checks.
+  sealed trait Result[T]
+  case class Unknown[T](support: Int, noSupport: Int) extends Result[T]
+  case class Passed[T]() extends Result[T]
+  case class Failed[T](value: Option[T]) extends Result[T]
 
   // A Property is a function that can be passed a set of parameters
-  // for a generator and tries to return a Evidence. To actually check a
-  // Property we need to call it multiple times and make sure it
+  // for a generator and tries to return a Evidence. To actually check
+  // a Property we need to call it multiple times and make sure it
   // never fails. The apply method of a given Property should return
   // Undecided when
-  trait Property[T] extends (Parameters => Option[Evidence[T]])
+  abstract class Property[T](
+    val predicate: Predicate[T],
+    val generator: Generator[T]
+  ) extends (Parameters => Option[Evidence[T]])
 
   object Property {
 
-    implicit def toProperty[T](p: Predicate[T])(implicit g: Generator[T]) = new Property[T] {
-      def apply(params: Parameters) = g(params).map { t => if (p(t)) Support(t) else Falsified(t) }
+    import Arbitrary._
+
+    implicit def toProperty[T](p: Predicate[T])(implicit g: Generator[T]) = new Property(p, g) {
+      def apply(params: Parameters) = generator(params).map { t =>
+        if (predicate(t)) Support(t) else Falsified(t)
+      }
     }
 
-    // Exists some value for which the predicate is true.
-    def exists[T](p: Predicate[T])(implicit g: Generator[T]) = new Property[T] {
-      def apply(params: Parameters) = g(params).map { t => if (p(t)) Proved(t) else Undecided(t) }
-    }
+    // Default conversion of a predicate to a property is to capture
+    // the appropriate generator and assert that the predicat should
+    // hold for all generated values.
+    def forAll[T](p: Predicate[T])(implicit g: Generator[T]) = toProperty(p)(g)
 
-    // For all values the predicate is true.
-    def forAll[T](p: Predicate[T])(implicit g: Generator[T]) = new Property[T] {
-      def apply(params: Parameters) = g(params).map { t => if (p(t)) Support(t) else Falsified(t) }
+    def forAll[
+      T1:Generator,
+      T2:Generator]
+      (fn: (T1, T2) => Boolean) = toProperty(fn.tupled)(arbitraryTuple[T1, T2])
+
+    def forAll[
+      T1:Generator,
+      T2:Generator,
+      T3:Generator]
+      (fn: (T1, T2, T3) => Boolean) = toProperty(fn.tupled)(arbitraryTuple[T1, T2, T3])
+
+    def forAll[
+      T1:Generator,
+      T2:Generator,
+      T3:Generator,
+      T4:Generator]
+      (fn: (T1, T2, T3, T4) => Boolean) = toProperty(fn.tupled)(arbitraryTuple[T1, T2, T3, T4])
+
+    // Less frequently used in tests, ocassionally it's useful to be
+    // able to assert that a predicate holds for at least one
+    // generated value.
+    def exists[T](p: Predicate[T])(implicit g: Generator[T]) = new Property(p, g) {
+      def apply(params: Parameters) = generator(params).map { t =>
+        if (predicate(t)) Proved(t) else Undecided(t)
+      }
     }
   }
 
-
-  def check[T](property: Property[T], params: Parameters): Result = {
-    @tailrec def loop(iters: Int, support: Int, trials: Int): Result = {
+  def check[T](property: Property[T], params: Parameters): Result[T] = {
+    @tailrec def loop(iters: Int, support: Int, trials: Int): Result[T] = {
       if (iters == 0) {
         Unknown(support, trials)
       } else {
@@ -67,8 +95,8 @@ object MonkeyCheck {
           opt.get match {
             case Undecided(_) => loop(iters - 1, support, trials + 1)
             case Support(_)   => loop(iters - 1, support + 1, trials + 1)
-            case Proved(_)    => Passed
-            case Falsified(_) => Failed
+            case Proved(_)    => Passed[T]
+            case Falsified(t) => Failed(Some(t))
           }
         } else {
           loop(iters -1, support, trials)
@@ -76,14 +104,16 @@ object MonkeyCheck {
       }
     }
 
-    loop(0, 0, 100) match {
-      case pass @ Passed => pass
-      case fail @ Failed => fail
+    loop(100, 0, 0) match {
+      case pass @ Passed() => pass
+      case fail @ Failed(_) => fail
       case Unknown(support, trials) =>
-        if ((support.toDouble / trials) > .8) Passed else Failed
+        if ((support.toDouble / trials) > .8) Passed[T] else {
+          println("Converting %d/%d to Failed".format(support, trials))
+          Failed[T](None)
+        }
     }
   }
-
 
   trait Generator[T] extends (Parameters => Option[T]) { outer =>
 
@@ -114,6 +144,7 @@ object MonkeyCheck {
 
   def arbitrary[T](ps: Parameters)(implicit g: Generator[T]): Option[T] = g.apply(ps)
   def generator[T](implicit g: Generator[T]): Generator[T] = g
+  def theGenerator[T](implicit g: Generator[T]): Generator[T] = g
   def between[T](min: T, max: T)(implicit b: Between[T]): Generator[T] = b(min, max)
   def oneOf[T](xs: Seq[T]): Generator[T] = between(0, xs.size - 1).map(xs(_))
   def oneOf[T](t: T, ts: T*): Generator[T] = oneOf[T](t +: ts)
@@ -199,7 +230,7 @@ object MonkeyCheck {
     ////////////////////////////////////////////////////////////////////////
     // Tuples
 
-    implicit def arbitraryTuple1[T1:Generator] =
+    implicit def arbitraryTuple[T1:Generator] =
       (for {
         t1 <- generator[T1]
       } yield Tuple1(t1)).shrinkWith {
@@ -207,7 +238,7 @@ object MonkeyCheck {
           generator[T1].shrink(t1).map(Tuple1(_))
       }
 
-    implicit def arbitraryTuple2[T1:Generator, T2:Generator] =
+    implicit def arbitraryTuple[T1:Generator, T2:Generator] =
       (for {
         t1 <- generator[T1]
         t2 <- generator[T2]
@@ -217,7 +248,7 @@ object MonkeyCheck {
           generator[T2].shrink(t2).map((t1, _))
       }
 
-    implicit def arbitraryTuple2[T1:Generator, T2:Generator, T3:Generator] =
+    implicit def arbitraryTuple[T1:Generator, T2:Generator, T3:Generator] =
       (for {
         t1 <- generator[T1]
         t2 <- generator[T2]
@@ -229,7 +260,7 @@ object MonkeyCheck {
           generator[T3].shrink(t3).map((t1, t2, _))
       }
 
-    implicit def arbitraryTuple4[T1:Generator, T2:Generator, T3:Generator, T4:Generator] =
+    implicit def arbitraryTuple[T1:Generator, T2:Generator, T3:Generator, T4:Generator] =
       (for {
         t1 <- generator[T1]
         t2 <- generator[T2]
@@ -304,5 +335,43 @@ object MonkeyCheck {
     implicit val betweenDouble: Between[Double] = (low: Double, high: Double) => new Generator[Double] {
       def apply(ps: Parameters) = choose(low, high, ps.random.nextDouble)
     }
+  }
+}
+
+object HelloWorld {
+  def main(args: Array[String]) {
+    import MonkeyCheck._
+    import MonkeyCheck.Property._
+
+
+    def show[T](r: Result[T]) = {
+      r match {
+        case Passed()                => println("okay!")
+        case Failed(t)               => println("whoops! Failed at: " + t.getOrElse("unknown"))
+        case Unknown(support, total) => println("What the heck?! Unknown result: %d/%d".format(support, total))
+      }
+    }
+
+    val params = Parameters(10, new Random)
+
+
+    def allNumbers() {
+      import MonkeyCheck.Arbitrary.Numbers._
+      val p = forAll { (i: Int) => i + 2 > i }
+      val p2 = forAll { (i: Int) => i - 2 > i }
+      show(check(p, params))
+      show(check(p2, params))
+      show(check((i: Int) => i * 10 > i, params))
+      show(check(forAll((i1: Int, i2: Int) => i1 * i2 == i2 * i1), params))
+    }
+
+    def positiveNumbers() {
+      import MonkeyCheck.Arbitrary.Numbers.+._
+      show(check(forAll((i1: Int, i2: Int) => i1 + i2 > i1 && i1 + i2 > i2), params))
+    }
+
+    allNumbers()
+    positiveNumbers()
+
   }
 }
